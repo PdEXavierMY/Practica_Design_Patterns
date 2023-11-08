f8df7638c85c10d50cef623d9571c384f97bfa9a
Xavitheforce <javier.miguelez.yague@gmail.com>
2023-11-08 10:48:59 +0100

    varias paginas

--- pizzeriaweb/pizzeriawebapp/urls.py
+++ pizzeriaweb/pizzeriawebapp/urls.py
@@ -3,4 +3,7 @@ from pizzeriawebapp import views
 
 urlpatterns = [
     path('', views.index, name='Home'),
+    path('login/', views.login, name='Login'),
+    path('register/', views.register, name='Registro'),
+    path('crear_pizza/', views.crear_pizza, name='Crear Pizza'),
 ]
--- pizzeriaweb/pizzeriawebapp/views.py
+++ pizzeriaweb/pizzeriawebapp/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'pizzeriawebapp/index.html')
+
+def login(request):
+    return render(request, 'pizzeriawebapp/login.html')
+
+def register(request):
+    return render(request, 'pizzeriawebapp/registro.html')
+
+def crear_pizza(request):
+    return render(request, 'pizzeriawebapp/crea_tu_pizza.html')
